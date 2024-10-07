13eadb1019539ecdedcc60beaee5f5fff9f7729c
Cyclops789 <70551889+Cyclops789@users.noreply.github.com>
2024-10-07 12:51:59 +0100

    Update

--- app/src/main/java/com/example/myapplication/Tutorials/Tuto_problems.kt
+++ app/src/main/java/com/example/myapplication/Tutorials/Tuto_problems.kt
@@ -1,5 +1,18 @@
 package com.example.myapplication.Tutorials
 
 fun main() {
+    // Print messages
     println("Use the val keyword when the value doesn't change. \nUse the var keyword when the value can change. \nWhen you define a function, you define the parameters that can be passed to it. \nWhen you call a function, you pass arguments for the parameters.")
+
+    // Fix compile errors
+    println("New chat message from a friend")
+
+    // String templates
+    var discountPercentage: Int = 0
+    var offer: String = ""
+    val item = "Google Chromecast"
+    discountPercentage = 20
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+
+    println(offer)
 }
