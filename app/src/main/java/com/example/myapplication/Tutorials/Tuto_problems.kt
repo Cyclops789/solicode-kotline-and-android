2ed3077e84f34b5f6844a6fe21cb12335cf5a71e
Cyclops789 <70551889+Cyclops789@users.noreply.github.com>
2024-10-07 12:50:19 +0100

    Update

---
+++ app/src/main/java/com/example/myapplication/Tutorials/Tuto_problems.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.Tutorials
+
+fun main() {
+    println("Use the val keyword when the value doesn't change. \nUse the var keyword when the value can change. \nWhen you define a function, you define the parameters that can be passed to it. \nWhen you call a function, you pass arguments for the parameters.")
+}
